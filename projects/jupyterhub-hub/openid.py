2e1d98df83194bf50e36a23473c8659312338b11
strangepleasures <pavel@thehyve.nl>
2020-04-15 15:22:58 +0200

    Remove debug logs

--- projects/jupyterhub-hub/openid.py
+++ projects/jupyterhub-hub/openid.py
@@ -150,10 +150,8 @@ class OpenIDConnectOAuthenticator(OAuthenticator):
                               validate_cert=self.tls_verify,
                               )
             resp = yield http_client.fetch(req)
-            self.log.error("Headers received from Saturn: %s", list(resp.headers.get_all()))
-            cookies = resp.headers.get_list('Set-Cookie')
 
-            for cookie in cookies:
+            for cookie in resp.headers.get_list('Set-Cookie'):
                 if cookie.startswith('JSESSIONID='):
                     session_id = cookie[len('JSESSIONID='):]
                     sep = session_id.find(';')
