2c0082e3aee69708b37c306ce78bc6b4c0cb1c33
strangepleasures <pavel@thehyve.nl>
2020-09-15 15:13:43 +0200

    Add Keycloak notification proxy

---
+++ projects/jupyterhub-singleuser/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+
+const GrantManager = require('keycloak-connect/middleware/auth-utils/grant-manager');
+const Grant = require('keycloak-connect/middleware/auth-utils/grant');
+const Token = require('keycloak-connect/middleware/auth-utils/token');
+
+const port = process.env.PORT || 80;
+const realmUrl = process.env.REALM_URL;
+const targetUrl = process.env.TARGET_URL;
+const clientId = process.env.CLIENT_ID;
+const secret = process.env.CLIENT_SECRET;
+const refresh_token = new Token(process.env.REFRESH_TOKEN, clientId);
+
+refresh_token.isExpired = () => false;
+
+const grantManager = new GrantManager({realmUrl, clientId, secret});
+const grant = new Grant({refresh_token});
+
+const app = express();
+app.set('trust proxy', true);
+
+const addToken = (proxyReq, req, res) => {
+    proxyReq.socket.pause();
+    grantManager.ensureFreshness(grant)
+        .then(grant => {
+            proxyReq.setHeader('Authorization', `Bearer ${grant.access_token.token}`);
+            proxyReq.socket.resume();
+        }).catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+    });
+}
+
+
+app.use(proxy('/**', {
+    target: targetUrl,
+    onProxyReq: addToken
+}));
+
+app.listen(port);
