168c54e542ad2133f55b0b403ff5dd81a0c8b8c6
ewelinagr <ewelina@thehyve.nl>
2021-11-19 16:17:40 +0100

    Update local development instruction including containerd testing.

--- local-development/hub/deploy.sh
+++ local-development/hub/deploy.sh
@@ -2,6 +2,7 @@
 
 here=$(realpath $(dirname "${0}"))
 helm_cmd=$(realpath ~/bin/helm3/helm)
+container_runtime="docker"
 
 # Prerequisites:
 # $ minikube start
@@ -10,12 +11,21 @@ helm_cmd=$(realpath ~/bin/helm3/helm)
 host_address=$(minikube ssh grep host.minikube.internal /etc/hosts | cut -f1)
 
 pushd "${here}"
-eval $(minikube docker-env) && \
+
+if [ "$container_runtime" = "docker" ]; then
+  eval $(minikube docker-env)
+fi
+
 (docker build ../../projects/jupyterhub-hub -t jupyterhub-hub-local:latest && docker build ../../projects/jupyterhub-singleuser -t jupyterhub-singleuser-local:latest) || {
   echo "Build failed."
   popd
   exit 1
 }
+
+if [ "$container_runtime" = "containerd" ]; then
+  minikube image load jupyterhub-hub-local:latest --logtostderr
+  minikube image load jupyterhub-singleuser-local:latest --logtostderr
+fi
 
 (kubectl get ns jupyterhub-dev || kubectl create ns jupyterhub-dev) && \
 ((${helm_cmd} repo list | cut -f1 | grep '^jupyterhub') || ${helm_cmd} repo add jupyterhub https://jupyterhub.github.io/helm-chart) && \
